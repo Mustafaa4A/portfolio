930dac7f92671a80a94879539bb3a42c8e742829
Mustafaa4a <musdhafa42@gmail.com>
2023-02-20 20:49:38 +0300

    building the app

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect
+from flask_frozen import Freezer
+import sys
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 
 @app.get('/')
@@ -15,4 +18,7 @@ def send():
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  if len(sys.argv) > 1 and sys.argv[1] == "build":
+    freezer.freeze()
+  else:
+    app.run(debug=True)
